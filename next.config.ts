c362d2a138c517f20fb9d026539175e67f0abebf
faytrix <faytrixdev@gmail.com>
2026-09-02 21:29:09 +0000

    fix: allow Google Analytics/Tag Manager in CSP
    - Add googletagmanager.com and google-analytics.com to script-src
    - Add google-analytics.com and googletagmanager.com to img-src
    - GA4 was blocked by Content-Security-Policy

--- next.config.ts
+++ next.config.ts
@@ -35,9 +35,9 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com",
               "style-src 'self' 'unsafe-inline'",
-              "img-src 'self' data: blob: https://*.supabase.co",
+              "img-src 'self' data: blob: https://*.supabase.co https://www.google-analytics.com https://www.googletagmanager.com",
               "font-src 'self' data:",
               "connect-src 'self' https://*.supabase.co https://*.googleapis.com https://*.r2.cloudflarestorage.com",
               "frame-src https://*.supabase.co",

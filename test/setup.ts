340bf9dd269df5f6cbcfc5a6bc23bb81dac31a5c
faytrix <faytrixdev@gmail.com>
2026-09-06 13:40:25 +0000

    test(setup): mock next/image for component tests

--- test/setup.ts
+++ test/setup.ts
@@ -1,1 +1,11 @@
+import React from "react";
 import "@testing-library/jest-dom/vitest";
+import { vi } from "vitest";
+
+vi.mock("next/image", () => ({
+  __esModule: true,
+  default: (props: Record<string, unknown>) => {
+    const { src, alt, ...rest } = props;
+    return React.createElement("img", { src, alt: alt ?? "", ...rest });
+  },
+}));
